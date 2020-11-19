574452bff57bbfcb48a6b5a0f92bc2bf5b209a13
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-11-19 09:16:55 -0500

    formating individual tasks

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,30 +1,26 @@
-import React from 'react'
-import TaskItem from './TaskItem'
-import Title from './Title'
+import React from "react";
+import TaskItem from "./TaskItem";
+import Title from "./Title";
 // import CustomCarousel from './CustomCarousel/CustomCarousel'
 
-
 function TaskList(props) {
-
-
-    return (
-            <div className="py-5">
-                <div className="container">
-                    <Title name="Our" title="Products" />
-                    {/* <div className="row"> */}
-                    {/* <ProductConsumer> */}
-                    {/* {value => {
+  return (
+    <div className="py-5">
+      <div className="container">
+        <Title name="Current" title="transport jobs" />
+        <div className="row">
+          {/* <ProductConsumer> */}
+          {/* {value => {
                                 return value.products.map(product => {
                                     return <ProductItem key={product.id} product={product} />
                                 })
                             }} */}
-                    <TaskItem />
-                    {/* </ProductConsumer> */}
-                    {/* </div> */}
-                </div>
-            </div>
-    )
-
+          <TaskItem />
+          {/* </ProductConsumer> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TaskList
+export default TaskList;
