3d856f52494e90534c39fe6f7aa05865d505c860
Andrew Dalba <andrewdalba@gmail.com>
2020-11-19 19:04:18 -0500

    changes userSchema values to not be required

--- models/user.js
+++ models/user.js
@@ -11,9 +11,9 @@ const userSchema = new Schema(
         // kyle added 12
         password: { type: String, required: true },
         date: { type: Date, default: Date.now },
-        role:{ type: String, required: true },
-        nameFirst:{ type: String, required: true },
-        nameLast:{ type: String, required: true },
+        role:{ type: String, required: false },
+        nameFirst:{ type: String, required: false },
+        nameLast:{ type: String, required: false },
         vesselName:{ type: String, required: false },
         position:{ type: String, required: false },
         profilePicture:{type: String, required: false }
